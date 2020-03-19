573413b9be0a0a7b832f6d8004ec83ca5e2da393
Muhammad Tifak <moht20009@gmail.com>
2020-03-19 05:12:11 -0700

    [ADD] scraper.js, selecting item/element by xpath

--- scraper.js
+++ scraper.js
@@ -8,7 +8,13 @@ async function scrapeArticle(url) {
   //to start up the browser, we need to wait for puppoeteer to launch the browser
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.goto(url);
+  await page.goto(url);
+
+  //element
+  //page is the page we opened, the $x is a puppeteer selector which allows us to select items
+  //by xpath; which is a way to naviagate the page (similar to JQuery or native JS libs) but 
+  //in a syntax that works better for webscrapers
+  const [el] = await page.$x('//*[@id="f74tgM1Y06gGTr"]');
 
 }
 
