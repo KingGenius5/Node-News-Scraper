df8043fe05efb817b28fe3e73d8c1af7bda12e8c
Muhammad Tifak <moht20009@gmail.com>
2020-03-23 18:14:18 -0700

    [UPDATE] scraper.js, using Expressfor RESTful APIs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express'); // Adding Express
+const app = express(); // Initializing Express
+
+const express = require('express'); // Adding Express
+const app = express(); // Initializing Express
+
+
+
+// Making Express listen on port 7000
+app.listen(7000, function () {
+  console.log(`Running on port 7000.`);
+});
